7adf01b70370d50994c263563f6a22d4ebd6e1da
Dinesh Nadakuditi <154011476+dinesh-nadakuditi@users.noreply.github.com>
2025-03-21 15:04:32 +0530

    Time: 7 ms (16.22%), Space: 44.1 MB (47.03%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,13 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
-        
-        int reversed = 0;
-        while (x > reversed) {
-            reversed = reversed * 10 + x % 10;
-            x /= 10;
-        }
-        
-        return x == reversed || x == reversed / 10;
+       String og = String.valueOf(x);
+       String rev = new StringBuilder(og).reverse().toString();
+       return og.equals(rev);
     }
 }
