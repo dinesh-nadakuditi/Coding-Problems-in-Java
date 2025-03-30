9b3b6740bb266aa03fa883bce2531b093c7c017e
Dinesh Nadakuditi <154011476+dinesh-nadakuditi@users.noreply.github.com>
2025-03-30 23:31:29 +0530

    Time: 0 ms (100%), Space: 41.1 MB (94.91%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int maxAltitude = 0, currentAltitude = 0;
+        for (int g : gain) {
+            currentAltitude += g;
+            maxAltitude = Math.max(maxAltitude, currentAltitude);
+        }
+        return maxAltitude;
+    }
+}
